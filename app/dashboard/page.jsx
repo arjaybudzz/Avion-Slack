3823d6bda1c2d65e90801b45eef6c3c5d66faeef
Arjay Onan <archessonan.1@gmail.com>
2023-10-31 20:05:45 +0800

    reduced top margin

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import ChatBox from '@/components/ChatBox'
-import Message from '@/components/Message'
+
 import Messages from '@/components/Messages'
 
 export default function Home() {
