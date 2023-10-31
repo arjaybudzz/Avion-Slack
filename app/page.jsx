54373d1ef9887403a71b525b96e660ae27f4512a
Arjay Onan <archessonan.1@gmail.com>
2023-10-31 19:51:45 +0800

    returned local storage

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,19 @@
+"use client"
 import { getStoredUser } from '@/storage/storeData';
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-
-  const storedUser = getStoredUser();
+  useEffect(() => {
+    const storedUser = getStoredUser();
 
     if (!storedUser) {
       router.push('auth/login'); // Redirect to the login page if not logged in
     } else {
       router.push('/dashboard'); // Redirect to the dashboard if logged in
-  }
+    }
+  }, []);
 
   return null;
 }
--- storage/storeData.js
+++ storage/storeData.js
@@ -1,16 +1,15 @@
-import { cookies } from 'next/headers'
 
 const auth = "auth-user";
 
 export function getStoredUser() {
-  const user = cookies().get(auth)?.value;
+  const user = localStorage.getItem(auth);
   return user ? JSON.parse(user) : null;
 }
 
 export function setStoredUser(user) {
-  cookies().set(auth, JSON.stringify(user));
+  localStorage.setItem(auth, JSON.stringify(user));
 }
 
 export function clearStoredUser() {
-  cookies().delete(auth);
+  localStorage.removeItem(auth);
 }
