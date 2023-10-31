c43bbb1c1eea72326bf30173f4b124b54fabffa2
Arjay Onan <archessonan.1@gmail.com>
2023-10-31 19:49:20 +0800

    removed use effect

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,16 @@
-"use client"
 import { getStoredUser } from '@/storage/storeData';
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    const storedUser = getStoredUser();
+
+  const storedUser = getStoredUser();
 
     if (!storedUser) {
       router.push('auth/login'); // Redirect to the login page if not logged in
     } else {
       router.push('/dashboard'); // Redirect to the dashboard if logged in
-    }
-  }, []);
+  }
 
   return null;
 }
